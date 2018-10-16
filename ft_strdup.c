61c9aa575aac206be0af5ef0a56564ab690dda27
Thomas Holzheu <tholzheu@e1z4r9p22.42.us.org>
2018-10-16 15:06:19 -0700

    yeah

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tholzheu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 19:50:55 by tholzheu          #+#    #+#             */
-/*   Updated: 2018/10/03 00:01:45 by tholzheu         ###   ########.fr       */
+/*   Updated: 2018/10/16 15:06:12 by tholzheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s1)
 	char	*new;
 	int		i;
 
+	if (!s1)
+		return (NULL);
 	i = 0;
 	new = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
 	if (!new)
